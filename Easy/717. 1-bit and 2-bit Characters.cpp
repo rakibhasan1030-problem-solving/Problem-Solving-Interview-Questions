35cbde110c1376a44af01c9ae32e8b5a082af6f5
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 18:06:13 +0600

    "Easy/717. 1-bit and 2-bit Characters.cpp": solved

--- Easy/717. 1-bit and 2-bit Characters.cpp
+++ Easy/717. 1-bit and 2-bit Characters.cpp
@@ -5,6 +5,15 @@ class Solution
 public:
     bool isOneBitCharacter(vector<int> &bits)
     {
-        
+        int len = bits.size();
+        bool ans = false;
+        for (int i = 0; i < len; i++)
+        {
+            if (i == len - 1)
+                ans = true;
+            if (bits[i] == 1)
+                i += 1;
+        }
+        return ans;
     }
 };
